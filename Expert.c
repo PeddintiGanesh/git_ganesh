e6a05d48805bc1f5a0deeeb40920159b6e87c8dc
PeddintiGanesh <157904191+PeddintiGanesh@users.noreply.github.com>
2024-02-19 09:49:07 +0530

    Update Expert.c

--- Expert.c
+++ Expert.c
@@ -1234,6 +1234,132 @@ void main()
     freeTree(root);
 }
 
+//12.A)PRIMS 
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int a, b, u, v, n, ne = 1, i, j, min, mincost = 0, cost[20][20], visited[10] = {0};
+    printf("\nEnter the number of nodes:");
+    scanf("%d", &n);
+    printf("\nEnter the weights of edges:\n");
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            scanf("%d", &cost[i][j]);
+            if (cost[i][j] == 0)
+            {
+                cost[i][j] = 999;
+            }
+        }
+    }
+    visited[1] = 1;
+    printf("\n");
+    while (ne < n)
+    {
+        for (i = 1, min = 999; i <= n; i++)
+        {
+            for (j = 1; j <= n; j++)
+            {
+                if (cost[i][j] < min)
+                {
+                    if (visited[i] != 0)
+                    {
+                        min = cost[i][j];
+                        a = u = i;
+                        b = v = j;
+                    }
+                }
+            }
+        }
+        if (visited[u] == 0 || visited[v] == 0)
+        {
+            printf("\nEdge %d:(%d %d) cost:%d", ne++, a, b, min);
+            mincost += min;
+            visited[b] = 1;
+        }
+        cost[a][b] = cost[b][a] = 999;
+    }
+    printf("\n%d", mincost);
+    getch();
+}
+
+//12.B)KRUSKALS
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int parent[10];
+int find(int i);
+int uni(int i,int j);
+void main()
+{
+   int ne=1,a,b,u,v,n,cost[20][20],i,j,min,mincost=0;
+   printf("\nEnter the number of vertices:");
+   scanf("%d",&n);
+   printf("\nENTER the weights of edges:");
+   for(i=1;i<=n;i++)
+   {
+      for(j=1;j<=n;j++)
+      {
+	 scanf("%d",&cost[i][j]);
+	 if(cost[i][j]==0)
+	 {
+	    cost[i][j]=999;
+	 }
+      }
+   }
+   while(ne<n)
+   {
+       for(i=1,min=999;i<=n;i++)
+       {
+	  for(j=1;j<n;j++)
+	  {
+	     if(cost[i][j]<min)
+	     {
+		 min=cost[i][j];
+		 a=u=i;
+		 b=v=j;
+	     }
+	  }
+       }
+
+   u=find(u);
+   v=find(v);
+   if(uni(u,v))
+   {
+      printf("\nEdge %d:(%d  %d) =%d\n",ne++,a,b,min);
+      mincost+=min;
+   }
+   cost[a][b]=cost[b][a]=999;
+}
+printf("\n%d\t",mincost);
+getch();
+}
+int find(int i)
+{
+   while(parent[i])
+
+      i=parent[i];
+
+   return i;
+}
+int uni(int i,int j)
+{
+  if(i!=j)
+  {
+    parent[j]=i;
+    return 1;
+  }
+	     return 0;
+}
+
+
 //16.A) Bubble sort in C:
 
 #include <stdio.h>

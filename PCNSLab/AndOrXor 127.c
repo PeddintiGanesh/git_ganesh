e14757d7c013619b5927e10d3b5187614cd7cc6d
PeddintiGanesh <157904191+PeddintiGanesh@users.noreply.github.com>
2024-06-25 08:08:41 +0530

    Update AndOrXor 127.c

--- PCNSLab/AndOrXor 127.c
+++ PCNSLab/AndOrXor 127.c
@@ -1,3 +1,12 @@
+/*
+
+Enter a string: Hello World
+Result of AND Operation: hello world
+Result of XOR Operation: hEllO wOrLd
+Result of OR Operation: HELLO WORLD
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -26,4 +35,45 @@ int main(){
 	}
 	printf("\n");
 	return 0;
+}
+
+
+//@2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char str[30], str1[30];
+    int i = 0, len;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  // Use fgets instead of gets for safety
+    str[strcspn(str, "\n")] = 0;  // Remove the newline character
+
+    len = strlen(str);
+
+    printf("Result of AND Operation: ");
+    for (i = 0; i < len; i++) {
+        str1[i] = str[i] & 127;
+        printf("%c", str1[i]);
+    }
+    printf("\n");
+
+    printf("Result of XOR Operation: ");
+    for (i = 0; i < len; i++) {
+        str1[i] = str[i] ^ 127;
+        printf("%c", str1[i]);
+    }
+    printf("\n");
+
+    printf("Result of OR Operation: ");
+    for (i = 0; i < len; i++) {
+        str1[i] = str[i] | 127;
+        printf("%c", str1[i]);
+    }
+    printf("\n");
+
+    return 0;
 }

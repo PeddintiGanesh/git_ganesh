31f56280eac358a754705c9727733fac38dd792e
PeddintiGanesh <157904191+PeddintiGanesh@users.noreply.github.com>
2024-07-25 21:10:16 +0530

    Update Queue.java

--- JavaLab (2)/Queue.java
+++ JavaLab (2)/Queue.java
@@ -1,90 +1,100 @@
 import java.util.Scanner;
-import java.lang.Exception;
 
-class QueueOverFlowException extends Exception {
-}
-
-class QueueUnderFlowException extends Exception {
-}
-
-class Queue {
-    int n = 5; // Maximum size of the queue.
-    int[] q = new int[n];
-    int front = -1;
-    int rear = -1;
-
-    void enqueue(int e) throws QueueOverFlowException {
-        if (rear == n - 1)
-            throw new QueueOverFlowException();
-        else {
-            rear++;
-            q[rear] = e;
-            System.out.println("Element Inserted");
-        }
-    }
-
-    void dequeue() throws QueueUnderFlowException {
-        if (front >= rear)
-            throw new QueueUnderFlowException();
-        else {
-            front++;
-            int e = q[front];
-            System.out.println("Element deleted from queue is : " + e);
-        }
-    }
-
-    void display() {
-        if (front == rear) {
-            System.out.println("Queue is empty");
-            return;
-        } else {
-            System.out.println("Elements qe : ");
-            for (int i = front + 1; i <= rear; i++)
-                System.out.print(q[i] + " ");
-            System.out.println();
-        }
+class QueueException extends Exception {
+    public QueueException(String message) {
+        super(message);
     }
 }
 
-class QDemo {
-    public static void main(String qgs[]) {
+class Queue1 {
+    private int[] queue;
+    private int front, rear, size, capacity;
+
+    public Queue1(int capacity) {
+        this.capacity = capacity;
+        queue = new int[capacity];
+        front = rear = size = 0;
+    }
+
+    // Add element to the queue
+    public void enqueue(int element) throws QueueException {
+        if (isFull()) {
+            throw new QueueException("Queue is full");
+        }
+        queue[rear] = element;
+        rear = (rear + 1) % queue.length;
+        size++;
+    }
+
+    // Remove element from the queue
+    public int dequeue() throws QueueException {
+        if (isEmpty()) {
+            throw new QueueException("Queue is empty");
+        }
+        int element = queue[front];
+        front = (front + 1) % queue.length;
+        size--;
+        return element;
+    }
+
+    // Check if queue is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Check if queue is full
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    // Display queue elements
+    public void display() {
+        System.out.print("Queue elements: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(queue[(front + i) % queue.length] + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class QueueDemo {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Queue x = new Queue();
-        System.out.println("1: Inserting element to queue(enqueue)");
-        System.out.println("2: Deleting element from queue(dequeue)");
-        System.out.println("3: Display all the elements of queue");
-        System.out.println("4: Exit");
-        int ch;
-        do {
-            System.out.println("Enter your choice : ");
-            ch = sc.nextInt();
-            switch (ch) {
-                case 1:
-                    System.out.println("enter element to be inserted:");
-                    int item = sc.nextInt();
-                    try {
-                        x.enqueue(item);
-                    } catch (QueueOverFlowException e) {
-                        System.out.println("Queue is Overflow Not possible to insert new element into the queue");
-                    }
-                    break;
-                case 2:
-                    try {
-                        x.dequeue();
-                    } catch (QueueUnderFlowException e) {
-                        System.out.println("Queue is Underflow Not possible to perform delete operation");
-                    }
-                    break;
-                case 3:
-                    x.display();
-                    break;
-                case 4:
-                    System.out.println("The End");
-                    break;
-                default:
-                    System.out.println("Invalid choice");
-                    sc.close();
+
+        System.out.print("Enter queue capacity: ");
+        int capacity = sc.nextInt();
+        Queue1 queue = new Queue1(capacity);
+
+        while (true) {
+            System.out.println("1. Enqueue");
+            System.out.println("2. Dequeue");
+            System.out.println("3. Display");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+            int option = sc.nextInt();
+
+            try {
+                switch (option) {
+                    case 1:
+                        System.out.print("Enter element to enqueue: ");
+                        int element = sc.nextInt();
+                        queue.enqueue(element);
+                        break;
+                    case 2:
+                        System.out.println("Dequeued element: " + queue.dequeue());
+                        break;
+                    case 3:
+                        queue.display();
+                        break;
+                    case 4:
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid option. Please choose a valid option.");
+                }
+            } catch (QueueException e) {
+                System.out.println("Exception caught: " + e.getMessage());
+                sc.close();
             }
-        } while (ch != 4);
+        }
     }
 }
